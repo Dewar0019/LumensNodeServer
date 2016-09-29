870cea94bc85802c09a7228aee654c956a17ce03
Dewar Tan <dewar0019@gmail.com>
2016-09-29 00:38:59 -0400

    pegging bluetooth on sever

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 //$ npm install --save public-ip
 
+
 var http = require('http');
 const publicIp = require('public-ip');
+const { spawn } = require('child_process')
+
+
+
 
 
 var setMacAddress = "EC:AD:B8:0A:BB:AD";
@@ -12,6 +17,17 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200, {"Content-Type": "application/json"});
  // var otherArray = ["success1", "success2"];
   //var otherObject = { item1: "success1val", item2: "success2val" };
+
+
+//deploySh.stdout.on('data', (data) => {
+//console.log("hi")
+ // console.log(`${data}`);
+//});
+spawn('sh', [ 'bt.sh' ], {
+  cwd: '/home/csfp/Desktop/LumenServer',
+  env: Object.assign({}, process.env, { PATH: process.env.PATH + ':/usr/local/bin' })
+})
+
   var json = JSON.stringify({
     "MAC": setMacAddress
   });
