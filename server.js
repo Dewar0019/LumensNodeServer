49fe78d33d5f75fe4715518a79ddcf5832e5e4ff
Dewar0019 <Dewar0019@gmail.com>
2016-10-27 00:11:09 -0400

    Cleanup and added more logging

--- server.js
+++ server.js
@@ -7,90 +7,95 @@ var utils = require('./utilities')
 const spawn = require('child_process').spawn;
 
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 
-app.get('/',function  (req, res) {
-   console.log("got a GET request for the homepage");
-   res.send('hello GET');
+app.get('/', function(req, res) {
+    console.log("got a GET request for the homepage");
+    res.send('hello GET');
 })
-app.post('/updateMe',function  (req, res) {
-   console.log("got a POST request for the homepage");
-   console.log("Lists of registered devices:");
-   console.log(req.body);
-   res.send('success');
+app.post('/updateMe', function(req, res) {
+    console.log("got a POST request for the homepage");
+    console.log("Lists of registered devices:");
+    console.log(req.body);
+    res.send('success');
 })
 
 
 var server = app.listen(8081, function() {
-   var host = server.address().address
-   var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-//Sending post request to update the list of registered devices
-var retrieveDevices = function() {
- unirest.post('http://192.168.1.82:8080/getStatus')
-.type('json')
-.send({
-deviceName: 'volen118'
-})
-.end(function (response) {
-if(response.body !== undefined) {
-  setMacAddress= response.body;
-}
-});
-}
-
-
-
-//Display public ip of node program
-publicIp.v4().then(ip => {
-    console.log("");
-    console.log("Server has started.");
-    console.log(ip + ":" + port);
-    console.log("");
-});
-
-
-
-
-// var setMacA
- // var timeout = setInterval(fuddress = "EC:AD:B8:0A:BB:AD";
-var setMacAddress = [];
-
-
-function searchBlueTooth() {
-for(var i =0; i<setMacAddress.length; i++) {
-	var currAddr = setMacAddress[i];
-  const deploySh = spawn('sh', [ 'bt.sh', currAddr ], {
-    cwd: '/home/csfp/Desktop/LumenServer',
-    env: Object.assign({}, process.env, { PATH: process.env.PATH + ':/usr/local/bin' })
-  });
-
-  //Results of the script
-  deploySh.stdout.on('data', (data) => {
-    //Remove linebreak at end of string
-    var returnStatus = (`${data}`).replace(/(\r\n|\n|\r)/gm,"");
-    console.log(returnStatus);
-    if(returnStatus == "RSSI") {
-	console.log("HELLO! " + setMacAddress[i]);
+    //Sending post request to update the list of registered devices
+    var retrieveDevices = function() {
+        unirest.post('http://192.168.1.82:8080/getStatus')
+            .type('json')
+            .send({
+                deviceName: 'volen118'
+            })
+            .end(function(response) {
+                if (response.body !== undefined) {
+                    setMacAddress = response.body;
+                }
+            });
     }
-  });
-}
-}
 
-//Runs the interval method to look for bluetooth connection
-(function() {
-  var timeout = setInterval(function() {
-      searchBlueTooth();
-      console.log("Updating lists of registered devices");
-      console.log(".");
-  }, (5000*setMacAddress.length)+1000);
 
-var retriever = setInterval(function() {
-      retrieveDevices();
-      console.log("Retrieving lists of registered devices");
-      console.log(".");
-  }, 10000);
-})();
+
+    //Display public ip of node program
+    publicIp.v4().then(ip => {
+        console.log("");
+        console.log("Server has started.");
+        console.log(ip + ":" + port);
+        console.log("");
+    });
+
+
+
+
+    // var setMacA
+    // var timeout = setInterval(fuddress = "EC:AD:B8:0A:BB:AD";
+    var setMacAddress = [];
+
+
+    function searchBlueTooth() {
+        for (var i = 0; i < setMacAddress.length; i++) {
+            console.log(setMacAddress).length;
+            var currAddr = setMacAddress[i];
+            const deploySh = spawn('sh', ['bt.sh', currAddr], {
+                cwd: '/home/csfp/Desktop/LumenServer',
+                env: Object.assign({}, process.env, {
+                    PATH: process.env.PATH + ':/usr/local/bin'
+                })
+            });
+
+            //Results of the script
+            deploySh.stdout.on('data', (data) => {
+                //Remove linebreak at end of string
+                var returnStatus = (`${data}`).replace(/(\r\n|\n|\r)/gm, "");
+                console.log(returnStatus);
+                if (returnStatus == "RSSI") {
+                    console.log("HELLO! " + setMacAddress[i]);
+                }
+            });
+        }
+    }
+
+    //Runs the interval method to look for bluetooth connection
+    (function() {
+        var timeout = setInterval(function() {
+            searchBlueTooth();
+            console.log("Updating lists of registered devices");
+            console.log(".");
+        }, (5000 * setMacAddress.length) + 1000);
+
+        var retriever = setInterval(function() {
+            retrieveDevices();
+            console.log("Retrieving lists of registered devices");
+            console.log(".");
+        }, 10000);
+    })();
 })
